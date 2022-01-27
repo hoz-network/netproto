d4623e452b2d6dff0590e3afdd94947455931374
František Novosad <frantisek@nvsd.dev>
2022-01-27 13:58:22 +0100

    Initial commit

---
+++ src/main/java/net/hoz/netproto/TimeUtils.java
@@ -0,0 +1,24 @@
+package net.hoz.netproto;
+
+import com.google.protobuf.Timestamp;
+import lombok.experimental.UtilityClass;
+
+import java.time.Instant;
+
+/**
+ * Utility class for converting {@link Instant} into {@link Timestamp} and via versa.
+ */
+@UtilityClass
+public class TimeUtils {
+
+    public Instant convert(Timestamp timestamp) {
+        return Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+    }
+
+    public Timestamp convert(Instant instant) {
+        return Timestamp.newBuilder()
+                .setSeconds(instant.getEpochSecond())
+                .setNanos(instant.getNano())
+                .build();
+    }
+}
